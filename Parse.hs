2e48a9a26ebd71ec50e9706c6fcbedf63b20f5bb
Evan Turner <evturn@gmail.com>
2017-12-05 04:08:09 -0500

    Add function to modify parse state

--- Parse.hs
+++ Parse.hs
@@ -4,6 +4,7 @@ import qualified Data.ByteString.Lazy       as L
 import qualified Data.ByteString.Lazy.Char8 as L8
 import           Data.Char                  (isSpace)
 import           Data.Int                   (Int64)
+import           Data.Word                  (Word8)
 
 
 data Greymap = Greymap
@@ -77,3 +78,28 @@ parse :: Parse a -> L.ByteString -> Either String a
 parse parser initState = case runParse parser (ParseState initState 0) of
                            Left err          -> Left err
                            Right (result, _) -> Right result
+
+modifyOffset :: ParseState -> Int64 -> ParseState
+modifyOffset initState newOffset = initState { offset = newOffset }
+
+parseByte :: Parse Word8
+parseByte = getState ==> \initState ->
+            case L.uncons (string initState) of
+              Nothing                -> bail "no more input"
+              Just (byte, remainder) -> putState newState ==> \_ ->
+                identity byte
+                  where
+                    newState = initState { string = remainder
+                                         , offset = newOffset
+                                         }
+                    newOffset = offset initState + 1
+
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right ((), s))
+
+bail :: String -> Parse a
+bail err = Parse $ \s ->
+           Left $ "byte offset " ++ show (offset s) ++ ": " ++ err
