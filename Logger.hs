8f0bca4dd94ffce2b53796eb15dc3d98747b5551
Evan Turner <ev@evturn.com>
2017-12-08 04:12:54 -0500

    Make Logger an instance of Monad

--- Logger.hs
+++ Logger.hs
@@ -18,6 +18,13 @@ runLogger = execLogger
 record :: String -> Logger ()
 record s = Logger ((), [s])
 
+instance Monad Logger where
+  return a = Logger (a, [])
+  m >>= k = let (a, w) = execLogger m
+                n      = k a
+                (b, x) = execLogger n
+             in Logger (b, w ++ x)
+
 globToRegex :: String -> Logger String
 globToRegex cs = globToRegex' cs >>= \ds -> return ('^':ds)
 
